61da211f563b6bf348b8ceae77ef7e6585efff52
Gove <govedot@gmail.com>
2023-09-01 03:00:00 +0100

    comments added

--- 01-set-matrix-zeros/01-brute/main.go
+++ 01-set-matrix-zeros/01-brute/main.go
@@ -4,7 +4,8 @@ func setZeroes(matrix [][]int) {
 	lCol := len(matrix)
 	lRow := len(matrix[0])
 
-	setZerosInRow := func(rowIdx int) {
+	// Sets non zero (>0) values in a given row to -1
+	markRowValues := func(rowIdx int) {
 		for j := 0; j < lRow; j++ {
 			if matrix[rowIdx][j] > 0 {
 				matrix[rowIdx][j] = -1
@@ -12,7 +13,8 @@ func setZeroes(matrix [][]int) {
 		}
 	}
 
-	setZerosInColumn := func(colIdx int) {
+	// Sets non zero (>0) values in a given column to -1
+	markColumnValues := func(colIdx int) {
 		for i := 0; i < lCol; i++ {
 			if matrix[i][colIdx] > 0 {
 				matrix[i][colIdx] = -1
@@ -20,17 +22,17 @@ func setZeroes(matrix [][]int) {
 		}
 	}
 
-	// Convert all non zero values to -1
+	// Mark all non zero values to -1
 	for i := 0; i < lCol; i++ {
 		for j := 0; j < lRow; j++ {
 			if matrix[i][j] == 0 {
-				setZerosInRow(i)
-				setZerosInColumn(j)
+				markRowValues(i)
+				markColumnValues(j)
 			}
 		}
 	}
 
-	// Convert all -1 values to 0
+	// Mark all -1 values to 0
 	for i := 0; i < lCol; i++ {
 		for j := 0; j < lRow; j++ {
 			if matrix[i][j] == -1 {
