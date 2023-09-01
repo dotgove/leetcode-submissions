58ccba53eb538bf38f9b229f48d528f48e102f85
Gove <govedot@gmail.com>
2023-09-01 02:00:00 +0100

    Brute ver

--- 01-set-matrix-zeros/01-brute/main.go
+++ 01-set-matrix-zeros/01-brute/main.go
@@ -1,4 +1,41 @@
 package main
 
 func setZeroes(matrix [][]int) {
+	lCol := len(matrix)
+	lRow := len(matrix[0])
+
+	setZerosInRow := func(rowIdx int) {
+		for j := 0; j < lRow; j++ {
+			if matrix[rowIdx][j] > 0 {
+				matrix[rowIdx][j] = -1
+			}
+		}
+	}
+
+	setZerosInColumn := func(colIdx int) {
+		for i := 0; i < lCol; i++ {
+			if matrix[i][colIdx] > 0 {
+				matrix[i][colIdx] = -1
+			}
+		}
+	}
+
+	// Convert all non zero values to -1
+	for i := 0; i < lCol; i++ {
+		for j := 0; j < lRow; j++ {
+			if matrix[i][j] == 0 {
+				setZerosInRow(i)
+				setZerosInColumn(j)
+			}
+		}
+	}
+
+	// Convert all -1 values to 0
+	for i := 0; i < lCol; i++ {
+		for j := 0; j < lRow; j++ {
+			if matrix[i][j] == -1 {
+				matrix[i][j] = 0
+			}
+		}
+	}
 }
