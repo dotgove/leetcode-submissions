e9b797627c343f2a8f6d0605813bcde502800745
Gove <govedot@gmail.com>
2023-09-03 00:00:00 +0100

    Halved TC

--- 01-set-matrix-zeros/02-better/main.go
+++ 01-set-matrix-zeros/02-better/main.go
@@ -18,36 +18,12 @@ func setZeroes(matrix [][]int) {
 		}
 	}
 
-	// Set the respective rows in matrix to 0
-	for i := 0; i < len(idxRows); i++ {
-		if idxRows[i] == 0 {
-			continue
-		}
+	// Consolidate setting zeros across rows and columns
+	for i := 0; i < nRows; i++ {
 		for j := 0; j < nCols; j++ {
-			matrix[i][j] = 0
-		}
-	}
-
-	// Set the respective cols in matrix to 0
-	for j := 0; j < nCols; j++ {
-		if idxCols[j] == 0 {
-			continue
-		}
-		for i := 0; i < nRows; i++ {
-			matrix[i][j] = 0
+			if idxRows[i] == 1 || idxCols[j] == 1 {
+				matrix[i][j] = 0
+			}
 		}
 	}
 }
-
-// func main() {
-// 	input := [][]int{
-// 		{0, 1, 2, 0},
-// 		{3, 4, 5, 2},
-// 		{1, 3, 1, 5},
-// 	}
-
-// 	setZeroes(input)
-// 	for _, row := range input {
-// 		fmt.Println(row)
-// 	}
-// }
