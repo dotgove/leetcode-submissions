391d62294ce2cc60eb62874eb30de7b9801d8552
Gove <govedot@gmail.com>
2023-09-07 03:00:00 +0100

    suboptimal solution

--- 04-maximum-subarray-lc53/solution.go
+++ 04-maximum-subarray-lc53/solution.go
@@ -1,5 +1,18 @@
 package maxsubarraylc53
 
 func maxSubArray(nums []int) int {
-	panic("Not Implemented")
+	allTimeMax := nums[0]
+	for i := 0; i < len(nums); i++ {
+		currentTotal := nums[i]
+		currentMax := currentTotal
+		for j := i + 1; j < len(nums); j++ {
+			if currentTotal += nums[j]; currentTotal > currentMax {
+				currentMax = currentTotal
+			}
+		}
+		if currentMax > allTimeMax {
+			allTimeMax = currentMax
+		}
+	}
+	return allTimeMax
 }
