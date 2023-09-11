ccfd974967de02fb09ffbc193801f514cbd8d56d
Gove <govedot@gmail.com>
2023-09-11 02:00:00 +0100

    suboptimal solution

--- 05-sort-colors-lc75/solution.go
+++ 05-sort-colors-lc75/solution.go
@@ -1,5 +1,25 @@
 package sortcolorslc75
 
+// Naive
 func sortColors(nums []int) {
-	panic("Not Implemented")
+	var color0, color1, color2 int
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			color0 += 1
+		} else if nums[i] == 1 {
+			color1 += 1
+		} else {
+			color2 += 1
+		}
+	}
+
+	for i := 0; i < color0; i++ {
+		nums[i] = 0
+	}
+	for i := color0; i < color0+color1; i++ {
+		nums[i] = 1
+	}
+	for i := color0 + color1; i < len(nums); i++ {
+		nums[i] = 2
+	}
 }
