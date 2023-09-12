e6efda468db5e5c93cf7254ed93fe770a67fcc46
Gove <govedot@gmail.com>
2023-09-12 01:00:00 +0100

    write tests

--- 06-best-time-to-buy-and-sell-stock-lc121/solution_test.go
+++ 06-best-time-to-buy-and-sell-stock-lc121/solution_test.go
@@ -1,1 +1,22 @@
 package besttimetobuyandsellstocklc121
+
+import "testing"
+
+func TestMaxProfit(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []int
+		expected int
+	}{
+		{"Example 1", []int{7, 1, 5, 3, 6, 4}, 5},
+		{"Example 2", []int{7, 6, 4, 3, 1}, 0},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := maxProfit(tc.input); got != tc.expected {
+				t.Errorf("maxProfit(%v) = %d; want = %d", tc.input, got, tc.expected)
+			}
+		})
+	}
+}
