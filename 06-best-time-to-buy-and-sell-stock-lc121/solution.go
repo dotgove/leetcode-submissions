a402d8b8d908d654e1e78b8f53d9e1d549c20f1d
Gove <govedot@gmail.com>
2023-09-13 00:00:00 +0100

    optimal solution

--- 06-best-time-to-buy-and-sell-stock-lc121/solution.go
+++ 06-best-time-to-buy-and-sell-stock-lc121/solution.go
@@ -1,5 +1,17 @@
 package besttimetobuyandsellstocklc121
 
 func maxProfit(prices []int) int {
-	panic("Not Implemented")
+	minPrice := prices[0]
+	maxProfit := 0
+
+	for currDay := 1; currDay < len(prices); currDay++ {
+		if prices[currDay] > minPrice {
+			if profit := prices[currDay] - minPrice; profit > maxProfit {
+				maxProfit = profit
+			}
+		} else {
+			minPrice = prices[currDay]
+		}
+	}
+	return maxProfit
 }
