14568003d5228b666397e12411ab9a5f38456cb3
Gove <govedot@gmail.com>
2023-09-13 03:00:00 +0100

    suboptimal solution

--- 07-two-number-sum-lc48/solution.go
+++ 07-two-number-sum-lc48/solution.go
@@ -1,5 +1,16 @@
 package twonumbersumlc48
 
-func twoSum(nums []int, target int) int {
-	panic("Not Implemented")
+// Naive
+func twoSum(nums []int, target int) []int {
+	result := make([]int, 2)
+loop:
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				result[0], result[1] = i, j
+				break loop
+			}
+		}
+	}
+	return result
 }
